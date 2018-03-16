c951dbd978a349d59e6db50c31b3c13a9a9bd5a8
manendra-PC\manendra <mannuatnri.itm@gmail.com>
2018-03-17 01:29:20 +0530

    react bolierplate with webpack :

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+
+      render(){
+       return (
+         <div className="container">
+            <div className="row">
+            <h3>Webpack + React App Demo </h3>
+            <div>Hello Manendra</div>
+            
+            </div>
+         </div>
+       )
+      }
+}
+
+export default App;
